81a866d04b8e509e6fa8c648d7257a137702ac11
Dimitar <dimitartsvetkov89@gmail.com>
2023-09-20 15:47:22 +0300

    [us-1]-set-up

---
+++ lib/gradient_container.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+const startAlignement = Alignment.topLeft;
+const endAlignement = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.colorsArray, {super.key});
+
+  final List<Color> colorsArray;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: colorsArray,
+          begin: startAlignement,
+          end: endAlignement,
+        ),
+      ),
+      child: Center(
+        child: Image.asset(
+          'assets/images/dice-2.png',
+          width: 200,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:first_app/gradient_container.dart';
+import 'package:flutter/material.dart';
+
+const List<Color> colorsArray = [
+  Color.fromARGB(255, 106, 49, 203),
+  Color.fromARGB(255, 135, 101, 194)
+];
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(colorsArray),
+      ),
+    ),
+  );
+}
